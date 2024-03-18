06fba0492190f1d12e12371a2625318cd3094bf0
andiigela <andiigela@gmail.com>
2024-03-18 11:08:22 +0100

    app commit

---
+++ FootballApp/src/main/java/com/football/dev/footballapp/FootballAppApplication.java
@@ -0,0 +1,13 @@
+package com.football.dev.footballapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballAppApplication.class, args);
+    }
+
+}
---
+++ FootballApp/src/test/java/com/football/dev/footballapp/FootballAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.football.dev.footballapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
