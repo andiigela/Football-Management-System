def383ee2752a7ecd3f81ef1bd3100d453a832b7
andiigela <andiigela@gmail.com>
2024-03-23 16:21:16 +0100

    feature/Authentication: Added app routes, and components that we will work in the future

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,15 +2,37 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import {RouterModule, Routes} from "@angular/router";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {ReactiveFormsModule} from "@angular/forms";
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import {AuthInterceptor} from "./services/auth.interceptor";
+const routes : Routes = [
+  {path: 'login',component:LoginComponent},
+  {path: 'dashboard',component:DashboardComponent}
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(routes),
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
