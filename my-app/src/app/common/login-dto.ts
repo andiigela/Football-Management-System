b1fc909f1b020c034df680056e1334a82646fa7f
andiigela <andiigela@gmail.com>
2024-03-23 16:24:10 +0100

    feature/Authentication: Created Login logic, retrieved access token and used it for authorization

---
+++ my-app/src/app/common/login-dto.ts
@@ -0,0 +1,4 @@
+export class LoginDto {
+    constructor(private username: string, private password: string) {
+    }
+}
---
+++ my-app/src/app/components/login/login.component.ts
@@ -0,0 +1,43 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
+import {LoginDto} from "../../common/login-dto";
+import {Router} from "@angular/router";
+import {AuthInterceptor} from "../../services/auth.interceptor";
+import {CookieService} from "ngx-cookie-service";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginObj: any = {
+    "username": "",
+    "password": ""
+  }
+  loginFormGroup: FormGroup = new FormGroup<any>('');
+
+  constructor(private formBuilder: FormBuilder,private http: HttpClient,private router: Router,private cookieService: CookieService) {
+
+  }
+
+  ngOnInit(): void {
+    this.loginFormGroup = this.formBuilder.group({
+      login: this.formBuilder.group({
+        username: new FormControl(''),
+        password: new FormControl('')
+      })
+    });
+    }
+    loginButton(): void {
+      let username:string = this.loginFormGroup.controls['login'].get('username')?.value;
+      let password:string = this.loginFormGroup.controls['login'].get('password')?.value;
+      let loginDto = new LoginDto(username,password);
+       this.http.post("http://localhost:8080/api/auth/login",loginDto,{withCredentials: true}).subscribe((res:any) =>{
+             AuthInterceptor.accessToken = res.accessToken;
+             document.cookie = `refreshToken=${res.refreshToken}`
+             this.router.navigateByUrl("/dashboard");
+       })
+    }
+}
