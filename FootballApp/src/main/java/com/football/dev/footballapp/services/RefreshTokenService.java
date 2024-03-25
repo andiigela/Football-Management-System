5c1fcacf377922265e61dc4a5919914acee42d49
andiigela <andiigela@gmail.com>
2024-03-25 15:46:16 +0100

    feature/Authentication: Finished refresh & access token code

---
+++ FootballApp/src/main/java/com/football/dev/footballapp/services/RefreshTokenService.java
@@ -0,0 +1,11 @@
+package com.football.dev.footballapp.services;
+
+import com.football.dev.footballapp.models.RefreshToken;
+
+import java.time.Instant;
+
+public interface RefreshTokenService {
+    RefreshToken createRefreshToken(String username, String token, Instant expiryDate);
+    RefreshToken findByToken(String token);
+    RefreshToken verifyExpiration(RefreshToken token);
+}
---
+++ FootballApp/src/main/java/com/football/dev/footballapp/services/RefreshTokenServiceImpl.java
@@ -0,0 +1,46 @@
+package com.football.dev.footballapp.services;
+
+import com.football.dev.footballapp.models.RefreshToken;
+import com.football.dev.footballapp.models.UserEntity;
+import com.football.dev.footballapp.repository.RefreshTokenRepository;
+import com.football.dev.footballapp.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+
+@Service
+public class RefreshTokenServiceImpl implements RefreshTokenService{
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final UserRepository userRepository;
+    public RefreshTokenServiceImpl(RefreshTokenRepository refreshTokenRepository,UserRepository userRepository){
+        this.refreshTokenRepository=refreshTokenRepository;
+        this.userRepository=userRepository;
+    }
+    public RefreshToken createRefreshToken(String username,String token, Instant expiryDate){
+        UserEntity user = userRepository.findByUsername(username).get();
+        if(user == null) return null;
+        RefreshToken existingRefreshDb = this.refreshTokenRepository.findByUserInfo(user);
+        if(existingRefreshDb == null){
+            RefreshToken newRefreshToken = new RefreshToken();
+            newRefreshToken.setToken(token);
+            newRefreshToken.setExpiryDate(expiryDate);
+            newRefreshToken.setUserInfo(user);
+            refreshTokenRepository.save(newRefreshToken);
+            return newRefreshToken;
+        }
+        existingRefreshDb.setToken(token);
+
+        refreshTokenRepository.save(existingRefreshDb);
+        return existingRefreshDb;
+    }
+    public RefreshToken findByToken(String token){
+        return this.refreshTokenRepository.findByToken(token);
+    }
+    public RefreshToken verifyExpiration(RefreshToken refreshToken){
+        if(refreshToken.getExpiryDate().compareTo(Instant.now())<0){
+            refreshTokenRepository.delete(refreshToken);
+            throw new RuntimeException(refreshToken.getToken() + " Refresh token is expired. Please make a new login..!");
+        }
+        return refreshToken;
+    }
+}
