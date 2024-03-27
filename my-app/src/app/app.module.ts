97bb767663ac7fd49b52228ac7493185ba68e2d8
andiigela <andiigela@gmail.com>
2024-03-27 15:19:13 +0100

    feature/Authentication: Finished Auth Guarding

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -10,9 +10,10 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import {AuthInterceptor} from "./services/auth.interceptor";
 import { LoginstatusComponent } from './components/loginstatus/loginstatus.component';
 import {AuthGuard} from "./services/auth.guard";
+import {AuthGuard2} from "./services/auth2.guard";
 const routes : Routes = [
   {path: 'login',component:LoginComponent,canActivate: [AuthGuard]},
-  {path: 'dashboard',component:DashboardComponent}
+  {path: 'dashboard',component:DashboardComponent,canActivate: [AuthGuard2]}
 ]
 
 
@@ -35,7 +36,8 @@ const routes : Routes = [
       useClass: AuthInterceptor,
       multi: true
     },
-    AuthGuard
+    AuthGuard,
+    AuthGuard2
   ],
   bootstrap: [AppComponent]
 })
