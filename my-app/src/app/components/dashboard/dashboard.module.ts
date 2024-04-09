856d43f10429a8c702e5a6c701f52bd694e7a517
Vesa <vf58160@ubt-uni.net>
2024-04-10 00:27:57 +0200

    feature/Club-Management: Activated AuthGuard to the new Dashboard Module

--- my-app/src/app/components/dashboard/dashboard.module.ts
+++ my-app/src/app/components/dashboard/dashboard.module.ts
@@ -4,13 +4,14 @@ import { DashboardComponent } from './dashboard.component';
 import { UsersComponent } from './users/users.component';
 import {NavbarComponent} from "./navbar/navbar.component";
 import {CommonModule} from "@angular/common";
-import {LoginstatusComponent} from "../loginstatus/loginstatus.component";
+import {AuthGuard2} from "../../services/auth2.guard";
 
 
 const routes: Routes = [
   {
     path: 'dashboard',
     component: DashboardComponent,
+    canActivate: [AuthGuard2],
     children: [
       { path: 'users', component: UsersComponent },
 
