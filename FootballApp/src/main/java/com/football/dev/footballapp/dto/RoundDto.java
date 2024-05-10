74f5b07842bfa938b20d36c5fabcf3c055f672c3
Vesa <vf58160@ubt-uni.net>
2024-05-10 08:26:40 +0200

    feature/Club-Management: Fixed a bug regarding Round creation

--- FootballApp/src/main/java/com/football/dev/footballapp/dto/RoundDto.java
+++ FootballApp/src/main/java/com/football/dev/footballapp/dto/RoundDto.java
@@ -14,6 +14,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RoundDto {
+    private Long id;
     private LocalDateTime start_date;
     private LocalDateTime end_date;
     private List<Match> matches;
--- FootballApp/src/main/java/com/football/dev/footballapp/mapper/RoundDtoMapper.java
+++ FootballApp/src/main/java/com/football/dev/footballapp/mapper/RoundDtoMapper.java
@@ -1,25 +1,16 @@
 package com.football.dev.footballapp.mapper;
 
-import com.football.dev.footballapp.dto.MatchDTO;
 import com.football.dev.footballapp.dto.RoundDto;
 import com.football.dev.footballapp.models.Round;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 @Component
 public class RoundDtoMapper implements Function<Round, RoundDto> {
 
-    private final MatchDTOMapper matchDtoMapper;
-
-    public RoundDtoMapper(MatchDTOMapper matchDtoMapper) {
-        this.matchDtoMapper = matchDtoMapper;
-    }
-
     @Override
     public RoundDto apply(Round round) {
-        return new RoundDto(round.getStart_date(), round.getEnd_date(), round.getMatches());
+        return new RoundDto(round.getId(),round.getStart_date(), round.getEnd_date(), round.getMatches());
     }
 }
