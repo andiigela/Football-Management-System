22c3e8731712b3370e4e7adb5f83e2d0c0f8ef1c
andiigela <andiigela@gmail.com>
2024-05-20 22:22:45 +0200

    feature/Player-Management: Configured Websocket in backend

---
+++ FootballApp/src/main/java/com/football/dev/footballapp/config/websocket/WebSocketSecurityConfiguration.java
@@ -0,0 +1,30 @@
+package com.football.dev.footballapp.config.websocket;
+import org.hibernate.Session;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.simp.SimpMessageType;
+import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
+import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.messaging.util.matcher.MessageMatcher;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketSecurityConfiguration extends AbstractSecurityWebSocketMessageBrokerConfigurer {
+    @Override
+    protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
+        messages
+                .nullDestMatcher().authenticated()
+                .simpDestMatchers("/app/playerDeleted").hasRole("ADMIN_CLUB")
+                .simpSubscribeDestMatchers("/topic/playerDeleted").hasRole("ADMIN_LEAGUE")
+                .anyMessage().permitAll();
+    }
+    @Override
+    protected boolean sameOriginDisabled() {
+        return true;
+    }
+
+}
