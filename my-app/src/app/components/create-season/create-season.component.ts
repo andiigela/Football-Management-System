63c82e80b06b038c9cca10e7e082e147efd97ad3
Vesa <vf58160@ubt-uni.net>
2024-05-20 15:48:32 +0200

    feature/Club-Management: Validation for creating and editing seasons

--- my-app/src/app/components/create-season/create-season.component.ts
+++ my-app/src/app/components/create-season/create-season.component.ts
@@ -4,36 +4,40 @@ import { SeasonService } from '../../services/season.service';
 import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
-    selector: 'app-create-season',
-    templateUrl: './create-season.component.html',
-    styleUrls: ['./create-season.component.css']
+  selector: 'app-create-season',
+  templateUrl: './create-season.component.html',
+  styleUrls: ['./create-season.component.css']
 })
 export class CreateSeasonComponent implements OnInit {
-    season: SeasonDto = new SeasonDto('', 0);
-    leagueId!: number;
+  season: SeasonDto = new SeasonDto('', 0);
+  leagueId!: number;
+  errorMessage: string | null = null;
 
-    constructor(
-        private seasonService: SeasonService,
-        private router: Router,
-        private route: ActivatedRoute // Inject ActivatedRoute
-    ) { }
+  constructor(
+    private seasonService: SeasonService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
-    ngOnInit(): void {
-        // Retrieve leagueId from route parameters
-        this.route.params.subscribe(params => {
-            this.leagueId = +params['id']; // Assuming 'leagueId' is the parameter name
-        });
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.leagueId = +params['id'];
+    });
+  }
+
+  createSeason(): void {
+    if (this.season.name.trim()) {
+      this.seasonService.createSeason(this.leagueId, this.season).subscribe(
+        () => {
+          console.log('Season created successfully');
+          this.router.navigate(['/league']);
+        },
+        error => {
+          console.log('Error creating season:', error);
+        }
+      );
+    } else {
+      this.errorMessage = 'Season name cannot be empty.';
     }
-
-    createSeason(): void {
-        this.seasonService.createSeason(this.leagueId, this.season).subscribe(
-            () => {
-                console.log('Season created successfully');
-                this.router.navigate(['/league']);
-            },
-            error => {
-                console.log('Error creating season:', error);
-            }
-        );
-    }
+  }
 }
