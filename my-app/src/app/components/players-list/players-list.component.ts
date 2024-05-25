1d676027a0a7510ece649ce7df6c1ec38488ef91
andiigela <andiigela@gmail.com>
2024-05-25 14:42:01 +0200

    feature/Player-Management: Created the logic in the client-side to change from ask permission to delete to waiting for permission in real time using websockets

--- my-app/src/app/components/players-list/players-list.component.ts
+++ my-app/src/app/components/players-list/players-list.component.ts
@@ -6,33 +6,66 @@ import {WebSocketService} from "../../services/web-socket.service";
 import {NotificationService} from "../../services/notification.service";
 import {NotificationDto} from "../../common/notification-dto";
 import {PlayerIdDto} from "../../common/player-id-dto";
+import {AuthService} from "../../services/auth.service";
 @Component({
   selector: 'app-players-list',
   templateUrl: './players-list.component.html',
   styleUrls: ['./players-list.component.css']
 })
-export class PlayersListComponent implements OnInit{
+export class PlayersListComponent implements OnInit, OnDestroy{
   playersList: PlayerDto[]=[];
   pageNumber: number = 1;
   pageSize: number = 10;
   totalElements: number = 0;
-  constructor(private playerService: PlayerService,private router: Router,private notificationService: NotificationService) {
+  private connectionId: string = "playeraskedpermission";
+  notificationsSentFromCurrentUser: PlayerIdDto[]=[];
+  playersWhoSentNotificationsFetched = false;
+  constructor(private playerService: PlayerService,private router: Router,
+              private webSocketService: WebSocketService,
+              private authService: AuthService) {
+  }
+  ngOnDestroy(): void {
+    this.webSocketService.disconnect(this.connectionId);
   }
   ngOnInit(): void {
+    this.webSocketService.connect(("/topic/askedpermission/"+this.authService.getUserIdFromToken()),this.connectionId);
+    this.subscribeToSentNotifications();
     this.getPlayers();
   }
-
+  private subscribeToRetrievedAskedPermissionPlayersFromApi(){
+    this.playerService.getPlayerIdsWhoAskedPermissionFromCurrentUser().subscribe((playerIds: PlayerIdDto[])=>{
+      this.playersList = this.playersList.map((playerDto: PlayerDto) => {
+        playerDto.permissionSent = playerIds.some((playerId: PlayerIdDto) => playerId.id == playerDto.id)
+        return playerDto;
+      })
+    })
+  }
+  private subscribeToSentNotifications(): void {
+    this.webSocketService.getMessages(this.connectionId).subscribe((data: any[]) => {
+      if(data.length > 0){
+        if (typeof data === 'string') {
+          this.notificationsSentFromCurrentUser = JSON.parse(data);
+          this.playersWhoSentNotificationsFetched=true;
+          console.log("Parsed notifications: ", this.notificationsSentFromCurrentUser);
+          this.updatePlayersListWithNotifications();
+        }
+      }
+    });
+  }
+  private updatePlayersListWithNotifications(): void {
+    this.playersList = this.playersList.map((playerDto: PlayerDto) => {
+      playerDto.permissionSent = this.notificationsSentFromCurrentUser
+          .some((playerIdDto: PlayerIdDto) => playerIdDto.id === playerDto.id
+      );
+      return playerDto;
+    });
+  }
   public getPlayers(){
     this.playerService.retrievePlayers(this.pageNumber-1,this.pageSize).subscribe((response)=>{
       this.playersList = response.content;
       this.totalElements = response.totalElements;
       this.updatePlayerList(this.playersList) // this.playersList, sepse kemi inicializu 2 rreshta me larte.
-      this.notificationService.retrieveNotificationsSentFromCurrentUser().subscribe((notifications: NotificationDto[]) => {
-        this.playersList = this.playersList.map((playerDto: PlayerDto) => {
-          playerDto.permissionSent = notifications.some((notificationDto: NotificationDto) => notificationDto.playerId==playerDto.id);
-          return playerDto;
-        })
-      })
+      this.subscribeToRetrievedAskedPermissionPlayersFromApi();
     })
   }
   OnPageChange(pageNumber: number){
